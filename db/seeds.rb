0645e97adb53183c920d578e50fc834d5988ee0e
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-07-06 18:35:51 +0530

    added test suite and helpers for the model Blog_Post

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Seed BlogPosts
+blog_posts = [
+  { title: "First Post", body: "This is the first post.", published_at: Time.now },
+  { title: "Second Post", body: "This is the second post.", published_at: Time.now }
+]
+
+blog_posts.each do |post_attributes|
+  BlogPost.find_or_create_by!(title: post_attributes[:title]) do |post|
+    post.body = post_attributes[:body]
+    post.published_at = post_attributes[:published_at]
+  end
+end
--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,41 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "draft? returns true for draft blog post" do
+    assert draft_post.draft?
+  end
+  test "draft? returns false for draft blog post" do
+    refute published_post.draft?
+  end
+  test "draft? returns false for scheduled blog post" do
+    refute scheduled_post.draft?
+  end
+  test "published? returns true for published blog post" do
+    assert published_post.published?
+  end
+  test "published? returns false for draft blog post" do
+    refute draft_post.published?
+  end
+  test "published? returns false for scheduled blog post" do
+    refute scheduled_post.published?
+  end
+  test "scheduled? returns true for scheduled blog post" do
+    assert scheduled_post.scheduled?
+  end
+  test "scheduled? returns false for draft blog post" do
+    refute draft_post.scheduled?
+  end
+  test "scheduled? returns false for published blog post" do
+    refute published_post.scheduled?
+  end
+
+  def draft_post
+    blog_posts(:draft)
+  end
+  def published_post
+    blog_posts(:published)
+  end
+  def scheduled_post
+    blog_posts(:scheduled)
+  end
 end
