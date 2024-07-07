1c0c50718c5d8d26403e5f3b71cd8ac830439b38
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-07-08 00:28:52 +0530

    invalid page pagination bug

--- app/controllers/blog_post_controller.rb
+++ app/controllers/blog_post_controller.rb
@@ -4,6 +4,10 @@ class BlogPostController < ApplicationController
   def index
     @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
     @pagy, @blog_posts = pagy(@blog_posts)
+    rescue Pagy::OverflowError
+      redirect_to root_path
+      # params[:page] = 1
+      # retry
   end
 
   def new
