59a17e33d4fca6b696ba80860f1f1c14536f2ed7
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-07-07 15:23:36 +0530

    used arel_sql to push all draft posts to the bottom of the list

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -2,7 +2,7 @@ class BlogPost < ApplicationRecord
   validates :title, presence: true, length: { maximum: 100 }
   validates :body, presence: true, length: { maximum: 1000 }
 
-  scope :sorted, -> { order(published_at: :desc, updated_at: :desc)}
+  scope :sorted, -> { order(arel_table[:published_at].desc.nulls_last).order(updated_at: :desc) }
   scope :draft, -> { where(published_at: nil)}
   scope :published, -> { where("published_at <= ?", Time.current)}
   scope :scheduled, -> { where(" published_at > ?", Time.current)}
